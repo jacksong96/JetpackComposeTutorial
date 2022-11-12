c20f30b6dbff0296ba24524c0bf46d124d665742
Jackson <jng111896@g.ucla.edu>
2022-11-12 01:58:32 -0800

    Created Image and Text composable

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.jetpackcomposetutorial
 
+import androidx.compose.foundation.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +35,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun JetpackTutorialWithImage(header: String, firstPara: String, secondPara: String){
+    val image = painterResource(R.drawable.bg_compose_background)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = "header image of mobile phone image with applications",
+            modifier = Modifier.fillMaxWidth()
+            )
+        JetpackTutorialWithText(header = header, firstPara = firstPara, secondPara = secondPara)
+        }
+}
+
+@Composable
+fun JetpackTutorialWithText(header: String, firstPara: String, secondPara: String) {
+    Column{
+        Text(text = header,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth().wrapContentWidth(Alignment.Start)
+                .padding(16.dp)
+        )
+        Text(text = firstPara,
+            fontSize = 16.dp,
+            modifier = Modifier.fillMaxWidth().wrapContentWidth(Alignment.Start).padding(start = 16.dp, end = 16.dp)
+        )
+        Text(text = secondPara,
+            fontSize = 16.dp,
+            modifier = Modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
